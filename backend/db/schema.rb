7e9c8b2e34817c0e911c0a0858a9696708dc6633
yuvaraj-6867 <yuvaraj6867@gmail.com>
2025-09-16 11:13:22 +0530

    Fix database setup and schema version

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_13_193503) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_13_193503) do
   create_table "automation_scripts", force: :cascade do |t|
     t.string "name"
     t.text "description"
