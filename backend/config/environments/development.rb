41a7714c20e17e19fcb67dab6f46b92b396ecccd
yuvaraj-6867 <yuvaraj6867@gmail.com>
2025-09-25 17:15:25 +0530

    Fix email configuration and enable SMTP for invitation emails

--- backend/config/environments/development.rb
+++ backend/config/environments/development.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD'],
+    user_name: 'yuvaraj6867@gmail.com',
+    password: 'ftab lzqq qhvo ktjx',
     authentication: 'plain',
     enable_starttls_auto: true
   }
